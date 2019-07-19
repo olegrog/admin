a1294d71c6f0ad67ed110252e180407e8da3fc7a
Oleg Rogozin <o.a.rogozin@gmail.com>
2019-07-19 22:13:43 +0300

    [cluster] colors & utils $ non-interactive

--- cdmm/configure_host.sh
+++ cdmm/configure_host.sh
@@ -3,12 +3,21 @@
 [[ $# -eq 0 ]] || { echo "Usage: $(basename "$0")"; exit 1; }
 [[ $EUID -eq 0 ]] || { echo "Run with sudo."; exit 1; }
 
+declare -xr RED='\033[1;31m'
+declare -xr GREEN='\033[1;32m'
+declare -xr YELLOW='\033[1;33m'
+declare -xr BLUE='\033[1;34m'
+declare -xr MAGENTA='\033[1;35m'
+declare -xr CYAN='\033[1;36m'
+declare -xr WHITE='\033[1;97m'
+declare -xr NC='\033[0m'
+
 header="# (cdmm cluster) $(printf '%(%Y-%m-%d)T\n' -1)"
 server=10.16.74.203
 admin=o.rogozin
 
-_log() { echo -e "\033[1;97m -- $*\033[0m"; }
-_topic() { echo -e "\033[1;31m === $* ===\033[0m"; }
+_log() { echo -e "$WHITE -- $*.$NC"; }
+_topic() { echo -e "$YELLOW=== $* ===$NC"; }
 
 _install() {
     declare -a packages not_installed
@@ -18,15 +27,32 @@ _install() {
     esac; done
     for pkg in "${packages[@]}"; do
         if dpkg -s "$pkg" > /dev/null 2>&1; then
-            _log "$pkg is already installed"
-            [[ $reconfigure ]] && dpkg-reconfigure "$pkg"
+            if [[ $reconfigure ]]; then
+                _log "Package $MAGENTA$pkg$WHITE is reconfigured"
+                dpkg-reconfigure -fnoninteractive "$pkg"
+            else
+                _log "Package $MAGENTA$pkg$WHITE is already installed"
+            fi
         else
             not_installed+=("$pkg")
         fi
     done
     if [[ ${#not_installed} -gt 0 ]]; then
-        _log "Install ${not_installed[*]}"
+        _log "Install $MAGENTA${not_installed[*]}$WHITE"
         apt-get install -y "${not_installed[@]}"
+    fi
+}
+
+_append() {
+    local file=$1
+    local line=$2
+    [[ -z "$line" ]] && { echo "An empty string is provided."; exit 1; }
+    # Write a header for all amendments to config files
+    [[ -f "$file" ]] || echo "$header" > "$file"
+    grep -q "$header" "$file" || { echo >> "$file"; echo "$header" >> "$file"; }
+    if ! grep -q "$line" "$file"; then
+        _log "Append to $MAGENTA$file$WHITE"
+        echo -e "$line" >> "$file"
     fi
 }
 
@@ -44,7 +70,8 @@ configure_ssh() {
 configure_ldap() {
     _topic "Configure LDAP client for NSS"
     _log "Set DebConf selections"
-    cat <<EOF | debconf-set-selections -v
+    echo PURGE | debconf-communicate ldap-auth-config > /dev/null
+    cat <<EOF | debconf-set-selections
 ldap-auth-config    ldap-auth-config/ldapns/base-dn     string      dc=cdmm,dc=skoltech,dc=ru
 ldap-auth-config    ldap-auth-config/ldapns/ldap-server string      ldap://$server
 ldap-auth-config    ldap-auth-config/dbrootlogin        boolean     false
@@ -52,22 +79,23 @@ libnss-ldapd        libnss-ldapd/nsswitch               multiselect passwd, grou
 EOF
     _install --reconfigure ldap-auth-config
     _install --reconfigure libnss-ldapd
+    if ! grep -q "hosts: *files ldap" /etc/nsswitch.conf; then
+        _log "Set LDAP priority higher than DNS for host NSS"
+        sed -i 's/^hosts:\( *\)files \(.*\) ldap$/hosts:\1files ldap \2/' /etc/nsswitch.conf
+    fi
+    if grep -q "pam_unix.so" /etc/pam.d/common-*; then
+        _log "Disable UNIX authentication"
+        sed -i '/.*pam_unix.so.*/d' /etc/pam.d/common-*
+        DEBIAN_FRONTEND=noninteractive pam-auth-update
+    fi
 }
 
 configure_nfs() {
-
     _topic "Configure NFS and autofs"
     _install nfs-common autofs
-    if ! grep cdmm /etc/auto.master; then
-        _log "Append /etc/auto.master"
-        echo -e "\n$header" >> /etc/auto.master
-        append=1
-    fi
     for dir in home opt; do
-        [[ $append ]] && printf '%-8s%s\n' "/$dir" "/etc/auto.$dir" >> /etc/auto.master
-        _log "Create /etc/auto.$dir"
-        echo "$header" > /etc/auto.$dir
-        printf '%-4s%s\n' "*" "$server:/$dir/&" >> /etc/auto.$dir
+        _append /etc/auto.master "$(printf '%-8s%s\n' "/$dir" "/etc/auto.$dir")"
+        _append /etc/auto.$dir "$(printf '%-4s%s\n' "*" "$server:/$dir/&")"
     done
     if [[ $(find /home -maxdepth 1 | wc -l) -gt 1 ]]; then
         if ! mount | grep -q "on /home "; then
@@ -88,15 +116,21 @@ configure_admins() {
     mkdir -p /root/.ssh
     chmod 700 /root/.ssh
     admin_key=$(cat /home/$admin/.ssh/id_rsa.pub)
-    if ! grep -q "$admin_key" /root/.ssh/authorized_keys; then
-        _log "Add $admin RSA key to /root/.ssh/authorized_keys"
-        echo "$admin_key" >> /root/.ssh/authorized_keys
-        chmod 500 /root/.ssh/authorized_keys
-    fi
+    _append /root/.ssh/authorized_keys "$admin_key"
+    chmod 500 /root/.ssh/authorized_keys
+}
+
+install_utils() {
+    _topic "Install additional utils"
+    _install environment-modules ack vim htop aptitude snapd
+    _append /etc/environment-modules/modulespath /opt/modules
+    _install python3-numpy python3-scipy python3-sympy python3-matplotlib
+    _install openmpi-common libopenmpi-dev
 }
 
 configure_ssh
 configure_ldap
 configure_nfs
 configure_admins
-
+install_utils
+_topic "All work has been successfully completed"
