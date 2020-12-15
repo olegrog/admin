ab944a15caaa019caf0f7cfb5dc50f674e24c202
Oleg Rogozin <o.a.rogozin@gmail.com>
2020-12-15 17:00:30 +0300

    [cdmm] do not update nVidia drivers

--- cdmm/get_active_users.sh
+++ cdmm/get_active_users.sh
@@ -6,21 +6,21 @@ source "$(dirname "$0")/common.sh"
 echo; _topic "Users with high CPU load"
 
 pdsh ps -eo pcpu,user:20,args \
-    | awk '{ if ($2 > 25) { if (h != $1) print "'$GREEN'"$1"'$NC'"; h=$1; $1=""; print }}'
+    | awk '{ if ($2 > 25) { if (h != $1) print "'"$GREEN"'"$1"'"$NC"'"; h=$1; $1=""; print }}'
 
 echo; _topic "Last login"
 
 lastlog_range() {
-    lastlog -b$1 -t$2 | grep -v Latest | grep -v root
+    lastlog -b"$1" -t"$2" | grep -v Latest | grep -v root
 }
 
 lastlog -b0 -t1 | head -1
-printf "$GREEN"; lastlog_range 0 1
-printf "$YELLOW"
+echo -en "$GREEN"; lastlog_range 0 1
+echo -en "$YELLOW"
 for i in $(seq 9); do
-    lastlog_range $i $((i+1))
+    lastlog_range "$i" "$((i+1))"
 done
-printf "$RED"; lastlog_range 10 100
-printf "$NC"
+echo -en "$RED"; lastlog_range 10 100
+echo -en "$NC"
 echo
 
