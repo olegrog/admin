f8731aab41d4230e2fef4713f4ac0d76606963c1
Oleg Rogozin <o.a.rogozin@gmail.com>
2021-03-09 20:37:56 +0300

    [cdmm] check, activate and turn on the slurm nodes

--- cdmm/check_nodes.sh
+++ cdmm/check_nodes.sh
@@ -4,6 +4,7 @@
 source "$(dirname "$0")/common.sh"
 
 [ -z "$WCOLL" ] && _err "Run with sudo -E"
+_is_master || _err "Run from the master host"
 
 check_ganglia() {
     _log "Check Ganglia monitors"
@@ -14,6 +15,24 @@ check_ganglia() {
             _log "Trying to restart"
             ssh "$host" systemctl restart ganglia-monitor
         fi
+    done
+}
+
+check_slurm() {
+    _log "Check whether nodes are ready for SLURM"
+    for host in $(_get_hosts); do
+        if ! sinfo --Node | grep -Fq "$host"; then
+            _warn "Host $GREEN$host$RED was out of list"
+            _log "Trying to activate it"
+            ssh "$host" systemctl restart slurmd
+            sinfo --Node | grep "$host"
+        fi
+    done
+    for host in $(sinfo --Node | grep down | cut -f1 -d' '); do
+        _warn "Host $GREEN$host$RED was down"
+        _log "Trying to wake it"
+        scontrol update nodename="$host" state=idle
+        sinfo --Node | grep "$host"
     done
 }
 
@@ -28,3 +47,4 @@ if pdsh 'systemctl list-units --state=failed' | grep failed; then
 fi
 
 check_ganglia
+check_slurm
